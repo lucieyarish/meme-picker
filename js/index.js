bd3ceaad139a0a49004cc88f1f98a48244db4995
Lucie Yarish <lucieyarish@hotmail.com>
2023-11-28 07:53:25 +0100

    Start adding logic for including/excluding gif memes

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import { catsData } from './data.js';
 
 const emotionRadios = document.getElementById('emotion-radios');
 const getImgBtn = document.getElementById('get-image-btn');
+const gifsOnlyOption = document.getElementById('gifs-only-option');
 
 function getEmotionsArray(cats) {
   const emotions = [];
@@ -54,9 +55,9 @@ function highlightCheckedOption(e) {
 }
 
 function getMatchingCatsArray() {
+  const isGif = gifsOnlyOption.checked;
   const checkedRadio = document.querySelector('input[type="radio"]:checked');
   if (checkedRadio) {
     const checkedRadioVal = checkedRadio.value;
-    console.log(checkedRadioVal);
   }
 }
